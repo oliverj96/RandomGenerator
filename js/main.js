bc9422a9530626990b6ff2c8aadac148e74c2bcf
Oliver Johnson <oliverjohnson@Olivers-MacBook-Air.local>
2018-09-12 14:53:34 -0500

    added more function to buttons

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
+document.getElementById("copyButton").onclick = copy;
+document.getElementById("clearButton").onclick = clear;
+document.getElementById("generateButton").onclick = generate;
+
 function generate(){
-    var text = "adffa";
+    var text = "adffa"; //placeholder
     document.getElementById("generatedText").value = text;
 }
 
 function copy(){
     //copy text to clipboard
+    var copyText = document.getElementById("generatedText").value;
+    //copyText.select();
+    document.execCommand("copy");
 }
 
 function clear(){
+    //clear generated text field
     document.getElementById("generatedText").value = "";
 }
 
-document.getElementById("copyButton").onclick = copy;
-document.getElementById("clearButton").onclick = clear;
-document.getElementById("generateButton").onclick = generate;
+//Functions below are to help the generate function
+
+function getLength(){
+    //return the length entered by the user
+}
