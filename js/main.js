088a1172cf24de82a86796d9c66e5fa2321e2f15
Oliver Johnson <oliverjohnson@Olivers-MacBook-Air.local>
2018-09-12 19:55:35 -0500

    Added random functionality

--- js/main.js
+++ js/main.js
@@ -4,14 +4,16 @@ function startScript(){
     document.getElementById("copyButton").onclick = copy;
     document.getElementById("clearButton").onclick = clear;
     document.getElementById("generateButton").onclick = generate;
+    document.getElementById("hideSpace").onclick = togleSpacingOptions;
 }
 
 function generate(){
-    var text = ""; //placeholder
+    var text = ""; //placeholderr 
+    var charSet = getCharacterSet();
     var length = getLength();
     for (let index = 0; index < length; index++) {
-        
-        text += "a";
+        var rnum = Math.floor(Math.random() * charSet.length);
+		text += charSet.substring(rnum,rnum+1);
     }
     document.getElementById("generatedText").value = text;
 }
@@ -28,6 +30,14 @@ function clear(){
     document.getElementById("generatedText").value = "";
 }
 
+//Additional functions
+
+function togleSpacingOptions(){
+    //toggle the options under spacing
+    isHidden = isSeperate();
+    document.getElementById("spacingOptions").hidden = !isHidden;
+}
+
 //Functions below are to help the generate function
 
 function getLength(){
@@ -38,10 +48,15 @@ function getLength(){
 
 function getCharacterSet(){
     //return array of characters used
+    var set1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var set2 = "abcdefghijklmnopqrstuvwxyz";
+    var set3 = "0123456789";
+    return set1;
 }
 
 function isSeperate(){
     //return the state of seperate
+    return document.getElementById("spacingOptions").hidden;
 }
 
 function serperateBy(){
